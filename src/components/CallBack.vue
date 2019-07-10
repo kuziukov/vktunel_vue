55090aacba5f4b23fd6be55b9d4e486990ba8374
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2019-07-11 00:06:10 +0300

    dadas

--- src/components/CallBack.vue
+++ src/components/CallBack.vue
@@ -5,17 +5,34 @@
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
     name: 'CallBack',
     props: {
         msg: String
     },
+    data(){
+        return{
+            access_token: '',
+            user_id: '',
+        }
+    },
     mounted: function () {
-        this.$nextTick(function () {
-            code = this.$route.query.code
-            console.log(code)
-            response = this.$store.dispatch('getAccessToken', code)
-            console.log(response)
+        self.access_token = this.$route.query.access_token
+        self.user_id = this.$route.query.user_id
+        
+        axios({url: self.url, method: 'GET' })
+        .then(resp => {
+          console.log(resp)
+          commit('auth_success', token, user)
+          resolve(resp)
+        })
+        .catch(err => {
+          commit('auth_error')
+          localStorage.removeItem('token')
+          reject(err)
         })
     }
 }
