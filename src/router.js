f6c3da178c4efac4251f9f984ee2fe2925c4e532
Dmitrii <dmitry.kuzyukov@arloid.com>
2019-08-07 15:56:04 +0300

    added nprogress

--- src/router.js
+++ src/router.js
@@ -83,6 +83,20 @@ let router = new Router({
     ]
   });
 
+router.beforeResolve((to, from, next) => {
+  // If this isn't an initial page load.
+  if (to.name) {
+    // Start the route progress bar.
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach((to, from) => {
+  // Complete the animation of the route progress bar.
+  NProgress.done()
+})
+
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
