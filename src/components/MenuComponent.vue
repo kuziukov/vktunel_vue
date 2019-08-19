b6f13e45d2918da5f4d4bbdaa482df5894576237
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2019-08-19 20:53:06 +0300

    changeses

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -54,7 +54,7 @@
         },
         methods: {
             login: function () {
-                window.location = 'https://oauth.vk.com/authorize?client_id=7029024&display=page&redirect_uri=http://localhost:8080/callback&scope=friends,photos,email,groups,offline&response_type=code&v=5.95';
+                window.location = 'https://oauth.vk.com/authorize?client_id=7029024&display=page&redirect_uri=http://wlusm.ru/callback&scope=friends,photos,email,groups,offline&response_type=code&v=5.95';
             }
         },
         beforeCreate() {
