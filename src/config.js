18ee2d55c5e8f7cd13caba01d8966ad1dff057d1
Dmitrii Kuziukov <kuzukov.dima@gmail.com>
2020-04-03 01:58:08 +0300

    config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     API: 'https://api.wlusm.ru/v1.0',
-    WEBSOCKET: 'ws://stream.wlusm.ru/',
+    WEBSOCKET: 'wss://stream.wlusm.ru/',
     files_url: 'https://api.wlusm.ru/files/',
     callback_url: 'https://wlusm.ru/callback'
 };
