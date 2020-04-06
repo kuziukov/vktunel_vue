9ea19543f4c6f39ad196a2e092f1823a2d7741c4
Kuziukov Dmitrii <kuziukov.dima@gmail.com>
2020-04-06 23:35:03 +0300

    fixed animations

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,18 @@
         }
     }
 
+    .fade-enter-active,
+    .fade-leave-active {
+        transition-duration: 0.3s;
+        transition-property: opacity;
+        transition-timing-function: ease;
+    }
+
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0
+    }
+
     .notifications{
         position: absolute;
     }
